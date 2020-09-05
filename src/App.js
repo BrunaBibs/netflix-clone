1e4a0c07b167ab85f0059561727971c855b2c430
Bruna Sousa <bruna_irina@hotmail.com>
2020-09-05 14:36:54 +0100

    prep for requests on the API

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ function App() {
   return (
     <div className="App">
       <h1>Let's build Netflix Clone</h1>
+      <Row title="Netflix Originals" />
+      <Row title="Trending Now" />
     </div>
+    
   );
 }
+
+
 
 export default App;
