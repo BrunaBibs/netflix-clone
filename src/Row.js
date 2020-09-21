513806cf032e10208b1b395fd0ab6f46b7df4752
Bruna Sousa <bruna_irina@hotmail.com>
2020-09-21 18:21:26 +0100

    fetching the images from the api

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 
+const base_url= "https://image.tmdb.org/t/p/original/";
+
 function Row({ title, fetchUrl }) {
 
     const [movies, setMovies] = useState([]);
@@ -9,17 +11,30 @@ function Row({ title, fetchUrl }) {
 
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request.data.results);
             setMovies(request.data.results);
             return request;
         }
         fetchData();
         // if [], run once when the row loads, and don't run it again
-    }, []);
+    }, [fetchUrl]);
+
+    console.log(movies);
+
+    // fetchUrl is inside the snippet because the variable is outside of the box, so useEffect knows that needs to refire that url
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+
+            <div className="row__posters">
+
+            {movies.map(movie => (
+
+                <img
+                className="row__poster"
+                src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+            ))}
+            </div>
 
         </div>
     )
