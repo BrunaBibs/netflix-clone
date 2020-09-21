3d29285533b020d5bbd80cea4d21262858624bfc
Bruna Sousa <bruna_irina@hotmail.com>
2020-09-21 17:56:00 +0100

    getting the api response from the URL

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
 
     const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+        // if [], run once when the row loads, and don't run it again
+    }, []);
 
     return (
         <div>
